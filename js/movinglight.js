db6f61dae90e68923f95e127d0c1b4c0d00fd57a
Luna <malalunapg@gmail.com>
2013-07-30 13:26:06 -0400

    Add Trackball libs and implementation moving camra whith mouse

--- js/movinglight.js
+++ js/movinglight.js
@@ -3,7 +3,7 @@
 // and every object are destruct rignt after the lauch of the function
 
 function launch(){
-    var camera, scene, renderer, cube, dirlight;
+    var camera, scene, renderer, cube, dirlight ;
     // Where do we display the sceen in th html DOM
     var $container ;
 
@@ -14,41 +14,47 @@ function launch(){
 
     function init(){
         // Scene Var
-        var SWidth = 800, 
-            SHeight = 700;
-        $container = $('#container');
+        // window.innerWidth / window.innerHeight
+       // var SWidth = window.innerWidth , 
+       //     SHeight = window.innerHeight;
 
+        var SWidth = 2200 , 
+            SHeight = 500;
+        $container = $('#container');
         // Camera Var
         // F = Frustrum : Quite difficulte to translate in french :P
         // FR : Aire totale de la pyramide lors que l'on coup 1/3 voir 1/4
         // de sa hauteur vers le pointu du cone.
         // En gros une pyramide sans pointe.
         // http://fr.wikipedia.org/wiki/Tronc_(g%C3%A9om%C3%A9trie)#Cas_du_c.C3.B4ne_et_de_la_pyramide
-        var View_Angle = 30
+        var ViewAngle = 40
             , Aspect = SWidth / SHeight
             , FNear = 1
-            , FFar = 1000;
+            , FFar = 10000;
 
         // Cube Var
-        var CWidth = 40
-            , CHeight = 40
-            , CDepth = 40;
+        var CWidth = 100
+            , CHeight = 100
+            , CDepth = 50;
 
-        renderer = new THREE.WebGLRenderer({
-            antialias       : true, // to get smoother output
-        });
+
+        // Instanciation of the canvas : If WebGL enabled : GO FOR IT !
+        if (window.WebGLRenderingContext){
+            renderer = new THREE.WebGLRenderer();
+            console.log("WEBGLRenderer !", renderer)
+            }
+        else{
+            renderer = new THREE.CanvasRenderer();
+            console.log("CanvaRenderer !", renderer)
+        }
         scene = new THREE.Scene();
+
+        /************** CUBE *************************/
         var cubeMaterial =
             new THREE.MeshLambertMaterial(
             {
                 color: 0xFFFFCC
             });// create a new mesh with cube geometry 
-        renderer.setClearColor(0xEEEEEE, 1.0);
-        renderer.clear();
-        renderer.shadowCameraFov = 50;
-        renderer.shadowMapWidth = 1024;;
-        renderer.shadowMapHeight = 1024;
-//        renderer.shadowMapEnabled = true;
         cube =  new THREE.Mesh( 
             new THREE.CubeGeometry(
                 CWidth
@@ -56,33 +62,54 @@ function launch(){
                 , CDepth)
             , cubeMaterial
         );
-        cube.castShadow = true;
-        cube.receiveShadow = true;
+//        cube.castShadow = true;
+//        cube.receiveShadow = true;
+
+        /******************** CAMERA *****************/
         // Camera Instanciation
-        camera = new THREE.OrthographicCamera( SWidth / - 2, SWidth / 2, SHeight / 2, SHeight / - 2, FNear, FFar );
+        camera = new THREE.PerspectiveCamera( ViewAngle, Aspect, FNear, FFar );
+        console.log(camera.position.x);
+        camera.position.z = 500;
+//        camera.position.x = radious * Math.sin( Cameratheta * Math.PI / 360 ) * Math.cos( Cameraphi * Math.PI / 360 );
+//        camera.position.y = radious * Math.sin(Cameraphi * Math.PI / 360 );
+//        camera.position.z = radious * Math.cos( Cameratheta * Math.PI / 360 ) * Math.cos( Cameraphi * Math.PI / 360 );
+        console.log(camera);
+
+
+        /************** SPOT LIGHT ************/
         var dirlight   = new THREE.SpotLight( 0x8888FF, 2 );
-        dirlight.target.position.set( 170, 300, -120 );
+        dirlight.target.position.set( 170, 300, 120 );
+        dirlight.intensity = 1.5
         dirlight.castShadow = true;
         var terre = new THREE.Mesh( 
-            new THREE.PlaneGeometry(400, 200, 1, 100)
+            new THREE.PlaneGeometry(300, 500, 1,2) 
         );
-//        terre.rotation.x = -Math.PI/2;
-        terre.position.y = -25.1;
+        terre.rotation.x = -Math.PI/3;
+        
+        terre.position.y = -120;
+        terre.position.z= 10;
         terre.receiveShadow = true;
+
+        //TrackballControls is a new library that control every single parameter
+        // theta, phi and all of this geometry braincrash of the mouse
+        // Big Up to you, http://egraether.com/
+        // You only have to attached an object to it to make it move with your mouse
+        controls = new THREE.TrackballControls( camera );
+        controls.target.set( 0, 0, 0 )
+        controls.panSpeed = 0.8;
+        controls.keys = [ 65, 83, 68 ];
 
         var light = new THREE.AmbientLight( 0x9999FF ); // soft white light
         scene.add( dirlight );
+        scene.add( light );
         scene.add(cube);
         scene.add(terre);
         // add the camera to the scene
         scene.add(camera);
         // the camera starts at 0,0,0
         // so pull it back
-      camera.position.z =100;
- //       camera.lookAt (scene.position);
-
-//       scene.add( light );
-//        console.log(dirlight);
+//       camera.lookAt (scene);
+        console.log(scene.position);
         // start the renderer
         renderer.setSize(SWidth, SHeight);
 
@@ -90,17 +117,15 @@ function launch(){
         $container.append(renderer.domElement);
 
         // before rendering the scene, lets move our cubes in the scene
+//       renderer.setClearColor(0xEEEEEE, 1.0);
+//        renderer.clear();
+//        renderer.shadowCameraFov = 50;
+//        renderer.shadowMapWidth = SWidth;
+//        renderer.shadowMapHeight = SHeight;
 
+//        renderer.shadowMapENabled = true;
         // render the seen
         renderer.render(scene, camera);
-        // binding mousewheel
-        $('#container').bind('mousewheel', function(event, delta, deltaX, deltaY) {
-            var fov =- deltaY*0.05;
-            camera.projectionMatrix = THREE.Matrix4.makePerspective( fov, window.innerWidth / window.innerHeight, 1, 1100 );
-
-//            camera.position.z =+ Math.floorMath.sin(deltaY )
-            console.log(delta, deltaX, deltaY);
-        });
     }
 
     // Animate function allow object to move with requestAnimationFrame
@@ -113,19 +138,12 @@ function launch(){
     function render (){
         // Get the time to move object every milliseconds
         var timer = new Date().getTime()  ;
-    cube.rotation.x =timer/1000;
-    cube.rotation.y = timer/1000;
-    
+        cube.rotation.x =timer/1000;
+        cube.rotation.y = timer/1000;
 
-        // Move the camera in a circle with the pivot point in the centre of this circle...
-        // ...so that the pivot point, and focus of the camera is on the centre of our scene.
-//        camera.position.x = Math.floor(Math.cos( timer )* 200);
-//        camera.position.y = Math.floor(Math.cos( timer )* 200);
-//        camera.position.z = Math.floor(Math.sin( timer ) * 200);
-        // Ask the camera to look at the scene.
-        // If you don't do it, the camera will shacking or look elsewhere.
-        // pretty cool to have it !!
         // render the scene after moving the camera           
+        controls.update();
         renderer.render(scene, camera);
     }
+
 } launch();
